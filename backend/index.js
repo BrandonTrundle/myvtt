d26f0e8f73a612a291878ed962553812aff14c6d
TrixieDreamz <trundle24@yahoo.com>
2025-04-20 08:24:16 +0200

    First commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// backend/index.js
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('VTT Backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-10 text-3xl text-blue-600 font-bold">
+      Tailwind is working! 🎨
+    </div>
+  );
+}
+
+export default App;
